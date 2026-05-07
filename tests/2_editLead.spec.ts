9f5a117f78bea96c2cec3c3eb59f498e7ba505d9
Loganathan P <52126070+Loganathan-7264@users.noreply.github.com>
2026-05-08 02:09:41 +0530

    Clear username and password fields in edit lead test
    Updated test to clear username and password fields before login.

--- tests/2_editLead.spec.ts
+++ tests/2_editLead.spec.ts
@@ -5,8 +5,8 @@ import { expect, test } from "@playwright/test";
 test("TC002: Edit Lead", async({page, context})=>{
     // Login
     await page.goto("https://apps.theauto-mate.com/crm/login.php");
-    await page.getByRole("textbox", {name:'Username'}).fill("test.automate");
-    await page.getByRole("textbox",{name:'Password'}).fill("test@123");
+    await page.getByRole("textbox", {name:'Username'}).fill("");
+    await page.getByRole("textbox",{name:'Password'}).fill("");
     await page.getByRole("button",{name:'Sign In'}).click();
     await page.getByText("Contacts / Leads").click();
     await page.locator("//p[text()='Tony']/ancestor::tr/td/following-sibling::td/button[text()='Edit']").first().click();
